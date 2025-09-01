072cc2d166b6f678c51d4db7211e85cd086e7cb0
Sintraspap Paraguay <sintraspappy@gmail.com>
2025-09-01 11:52:44 -0300

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,13 @@
 module.exports = function(eleventyConfig) {
+  // Copiar tal cual al output
   eleventyConfig.addPassthroughCopy("data");
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("assets");
-  return { dir: { input: ".", output: "_site" } };
+
+  return {
+    dir: {
+      input: ".",      
+      output: "_site", 
+    },
+  };
 };
