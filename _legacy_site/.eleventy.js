946340483fd7f03c6842842135660fe67efb7c9e
Sintraspap Paraguay <sintraspappy@gmail.com>
2025-09-05 13:21:27 -0300

    Delete _legacy_site directory

--- _legacy_site/.eleventy.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("data");
-  eleventyConfig.addPassthroughCopy("images");
-  eleventyConfig.addPassthroughCopy("assets");
-  return { dir: { input: ".", output: "_site" } };
-};
