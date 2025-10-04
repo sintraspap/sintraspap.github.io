68e357e3afc9df325499099d0c0f2a314e36ebff
Sintraspap Paraguay <sintraspappy@gmail.com>
2025-10-04 20:31:58 -0300

    4

---
+++ netlify-identity.js
@@ -0,0 +1,10 @@
+// netlify-identity.js
+if (window.netlifyIdentity) {
+  window.netlifyIdentity.on("init", user => {
+    if (!user) {
+      window.netlifyIdentity.on("login", () => {
+        document.location.href = "/admin/";
+      });
+    }
+  });
+}
